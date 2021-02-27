9875f6d41446a047be77c5cd7894140f7baec1a7
dipeshkoirala <dipeshkoirala@gmail.com>
2021-02-26 21:15:59 -0500

    Committing change on model router mw and migtation

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,31 @@
+const db=require("../../data/db-config")
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return Promise.resolve(db("cars"))
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  // DO YOUR MAGIC\
+  const car = db("cars").find(d => d.id === id)
+  return Promise.resolve(car)
+// }
 }
 
-const create = () => {
+ const  create = ({vin,make,model,mileage,title,transmission}) => {
   // DO YOUR MAGIC
+  
+  const newCar = { vin,make,model,mileage,title,transmission }
+  db("cars").push(newCar)
+  Promise.resolve(newCar)
 }
+
+module.exports = {
+	getAll,
+	getById,
+create,
+
+}
+
+
+
--- api/server.js
+++ api/server.js
@@ -1,6 +1,13 @@
 const express = require("express")
-
+const carRouter=require("./cars/cars-router")
 const server = express()
+
+server.use(express.json())
+
+server.use("/api/cars",carRouter)
+server.get("/",(req,res)=>{
+    res.send("hello from the server")
+})
 
 // DO YOUR MAGIC
 
